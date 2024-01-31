c68b249d5e66247159080933222c2512a0b7cfeb
athel99 <athel99@naver.com>
2024-01-31 15:16:14 +0900

    움직이는 방향에 따라 플에이어 이미지 반전

--- Assets/CatController.cs
+++ Assets/CatController.cs
@@ -1,31 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rbody;
-    [SerializeField] private float force = 680f;
-    
+    [SerializeField] private float jumpforce = 680f;
+    [SerializeField] private float moveforce = 0.003f;
+    [SerializeField] private CloudGamedirector gameDirector;
+
+    private void Start()
+    {
+        //this.gameDirector = GameObject.Find("GameDirector").GetComponent<ClimbCloudGameDirector>();
+        //this.gameDirector = GameObject.FindAnyObjectByType<ClimbCloudGameDirector>();
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        
         //점프
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            this.rbody.AddForce(this.transform.up * this.force);//회전방향대로 업
+            this.rbody.AddForce(this.transform.up * this.jumpforce);//회전방향대로 업
             //this.rbody.AddForce(Vector3.up * this.force); //직선위로만 업
-              }
+            //위는 캐릭터의 y축으로 날아가고 아래는 세계의 y축방향으로 날아간다.
+        }
+        
+        
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        //키를 누르고 있는 동안에 좌우이동
+        int dirX =0;
+        
+
+
+        if (Input.GetKey(KeyCode.RightArrow)) 
         {
-            this.rbody.AddForce(Vector3.left*this.force);
+            dirX = 1;
+           
 
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            this.rbody.AddForce(Vector3.right*this.force);
+
+        if (Input.GetKey(KeyCode.LeftArrow)) 
+        { 
+            dirX = -1; 
+           
 
         }
+
+        
+
+        if (dirX!= 0)
+        {
+            this.transform.localScale = new Vector3(dirX, 1, 1);
+
+        }
+
+
+        // Debug.Log(dir); //방향
+
+        //속도를 제한하자 
+
+
+        if (Mathf.Abs(this.rbody.velocity.x) < 3)
+        {
+            this.rbody.AddForce(this.transform.right * dirX * moveforce);
+        }
+             
+               
+        // Gamedirector
+        this.gameDirector.UpdateVelocityText(this.rbody.velocity);
+
 
 
     }
---
+++ Assets/Scripts/CloudGamedirector.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CloudGamedirector : MonoBehaviour
+{
+    [SerializeField] private Text velocityText;
+
+
+    public void UpdateVelocityText(Vector2 velocity)
+    {
+        float velocityX = Mathf.Abs(velocity.x);
+        this.velocityText.text = velocityX.ToString();
+      
+    }
+
+    
+}
